7866132d7923aa37ac8b60a31e53345e52e110bb
ticalceducation <aneeshbonthala15@gmail.com>
2023-06-19 15:09:18 -0700

    title page unpositioned

---
+++ src/Welcome.js
@@ -0,0 +1,16 @@
+import './styles.css';
+
+function Welcome() {
+  return (
+    <body>
+      <header className="Header1">
+          Start tracking today
+      </header>
+        <p className="NormalText">
+          Click anywhere to continue
+        </p>
+    </body>
+  );
+}
+
+export default Welcome;
