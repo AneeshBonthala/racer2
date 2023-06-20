1d46ab9b10af14031cbf4576b5261ad14f8360a8
ticalceducation <aneeshbonthala15@gmail.com>
2023-06-20 15:14:59 -0700

    homepage done for now

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,16 +1,29 @@
+import { useCallback } from "react";
+import Background from "./Background.js";
+import { loadFull } from "tsparticles";
 import './styles.css';
+import { motion } from "framer-motion"
 
-function HomePage() {
-  return (
-    <body>
-      <header className="start_tracking">
-          Start tracking today
-      </header>
-        <p className="click_anywhere">
-          Click anywhere to continue
+const HomePage = () => {
+  const handleClick = () => {
+    console.log('mouse click');
+  }
+    return (
+      <body onClick={handleClick}>
+        <Background></Background>
+        <header className="start_tracking">
+          <motion.div
+            whileHover={{ scale: 1.2, opacity: 0.5 }}
+          >Start tracking today
+          </motion.div>
+        </header>
+        <p className="click_here">
+          <motion.div
+            whileHover={{ scale: 1.1, opacity: 0.5 }}
+          >Click here to continue</motion.div>
         </p>
-    </body>
-  );
-}
+      </body>
+    );
+};
 
 export default HomePage;
