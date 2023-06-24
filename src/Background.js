2b7b353508b5b67cd722548e3025b28aeb25e49f
ticalceducation <aneeshbonthala15@gmail.com>
2023-06-24 10:41:13 -0700

    conditional allow  touch prop

--- src/Background.js
+++ src/Background.js
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
-const Background = () => {
+function Background(props) {
     const particlesInit = useCallback(async engine => {
         console.log(engine);
         // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
@@ -15,82 +15,44 @@ const Background = () => {
         await console.log(container);
     }, []);
 
-    return (
+    if (props.allowTouch == "true") return (
         <Particles
             id="tsparticles"
             init={particlesInit}
             loaded={particlesLoaded}
-            options={{
-                background: { 
-                    color: { 
-                        value: "#0C146A", 
-                    },
-
-                },
-                fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 4,
-                        },
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4,
-                        },
-                    },
-                },
-                particles: {
-                    color: {
-                        value: "#ffffff",
-                    },
-                    links: {
-                        color: "#ffffff",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    collisions: {
-                        enable: false,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: false,
-                        speed: 3,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 1200,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 5 },
-                    },
-                },
-                detectRetina: true,
-            }}
+            options={{background: { color: { value: "#0C146A", },},
+            fpsLimit: 120,
+            interactivity: {events: {onClick: {enable: true, mode: "push"},resize: true,},modes: {push: {quantity: 4,},repulse: {distance: 200,duration: 0.4,},},},
+                particles: {color: {value: "#ffffff",},
+                    links: {color: "#ffffff",distance: 150,enable: true,opacity: 0.5,width: 1,},
+                    collisions: {enable: false,},
+                    move: {direction: "none",enable: true,outModes: {default: "bounce",},random: false,speed: 3,straight: false,},
+                    number: {density: {enable: true,area: 1200,},value: 80,},
+                    opacity: {value: 0.5,},
+                    shape: {type: "circle",},
+                    size: {value: { min: 1, max: 5 },},},
+            detectRetina: true,}}
         />
     );
+    else return(
+        <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{background: { color: { value: "#0C146A", },},
+        fpsLimit: 120,
+        interactivity: {modes: {push: {quantity: 4,},repulse: {distance: 200,duration: 0.4,},},},
+            particles: {color: {value: "#ffffff",},
+                links: {color: "#ffffff",distance: 150,enable: true,opacity: 0.5,width: 1,},
+                collisions: {enable: false,},
+                move: {direction: "none",enable: true,outModes: {default: "bounce",},random: false,speed: 3,straight: false,},
+                number: {density: {enable: true,area: 1200,},value: 80,},
+                opacity: {value: 0.5,},
+                shape: {type: "circle",},
+                size: {value: { min: 1, max: 5 },},},
+        detectRetina: true,}}
+    />
+    )
 };
 
 export default Background;
